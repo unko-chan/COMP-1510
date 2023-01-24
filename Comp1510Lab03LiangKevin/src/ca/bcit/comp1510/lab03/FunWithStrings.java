91ba0fc791fe767566ce82208d2f30b01c2014be
unko-chan <kliang30@my.bcit.ca>
2023-01-24 10:42:38 -0800

    a

--- Comp1510Lab03LiangKevin/src/ca/bcit/comp1510/lab03/FunWithStrings.java
+++ Comp1510Lab03LiangKevin/src/ca/bcit/comp1510/lab03/FunWithStrings.java
@@ -2,16 +2,32 @@ package ca.bcit.comp1510.lab03;
 
 import java.util.Scanner;
 
-public class FunWithStrings {
-    public FunWithStrings() {
-    }
+/**
+ * Having fun with strings.
+ *
+ * @author Kevin Liang
+ * @version 2023
+ */
 
+public class FunWithStrings {
+
+    /**
+     * Length of word "The".
+     */
+    static final int CHAR = 3;
+
+    /**
+     * Drives the program.
+     *
+     * @param args unused.
+     */
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("enter the title of your favourite book.");
         String input = scan.nextLine();
-        if (input.substring(0, 3).equals("The")) {
-            System.out.println("The first word of your title starts with \"The\".");
+        if (input.substring(0, CHAR).equals("The")) {
+            System.out.println("The first word of your title starts with "
+                    + "\"The\".");
         }
 
         System.out.println(input.toUpperCase());

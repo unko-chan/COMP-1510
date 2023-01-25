f9147f4e4475a4b48708bd1d91b9ab3b65a7b101
unko-chan <kliang30@my.bcit.ca>
2023-01-25 11:18:24 -0800

    finish funwithstrings

--- Comp1510Lab03LiangKevin/src/ca/bcit/comp1510/lab03/FunWithStrings.java
+++ Comp1510Lab03LiangKevin/src/ca/bcit/comp1510/lab03/FunWithStrings.java
@@ -12,11 +12,6 @@ import java.util.Scanner;
 public class FunWithStrings {
 
     /**
-     * Length of word "The".
-     */
-    static final int CHAR = 3;
-
-    /**
      * Drives the program.
      *
      * @param args unused.
@@ -24,14 +19,50 @@ public class FunWithStrings {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("enter the title of your favourite book.");
+
         String input = scan.nextLine();
-        if (input.substring(0, CHAR).equals("The")) {
+
+        String trimmedUserInput = input.trim();
+
+        String inputUpperCase = trimmedUserInput.toUpperCase();
+        // 16. toUpperCase() does not mutate the original string.
+
+        final int inputLength = trimmedUserInput.length();
+
+        String trimmedLowerCase = trimmedUserInput.toLowerCase();
+
+        String firstUpper =
+                trimmedLowerCase.substring(0, 1).toUpperCase();
+
+        String lastUpper =
+                trimmedLowerCase.substring(inputLength - 1, inputLength).
+                        toUpperCase();
+
+        String trimmedString =
+                trimmedUserInput.substring(1, inputLength - 1).toLowerCase();
+
+        String finalString = firstUpper + trimmedString + lastUpper;
+
+        System.out.println("Stored Input: " + input);
+        System.out.println("Length of title: " + inputLength);
+
+        if (trimmedUserInput.startsWith("The")) {
             System.out.println("The first word of your title starts with "
                     + "\"The\".");
         }
 
-        System.out.println(input.toUpperCase());
-        String inputUpperCase = input.toUpperCase();
-        System.out.println(inputUpperCase);
+        System.out.println("toUpperCase() invoked on input: "
+                + trimmedUserInput.toUpperCase());
+
+        System.out.println("Assigned result to new String Variable: "
+                + inputUpperCase);
+
+        System.out.println("Name of book in uppercase: "
+                + trimmedUserInput.toUpperCase());
+
+        System.out.println("Name of book in lowercase: "
+                + trimmedUserInput.toLowerCase());
+
+        System.out.println(finalString);
     }
 }
